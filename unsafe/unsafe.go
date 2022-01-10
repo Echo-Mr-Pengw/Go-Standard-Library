0da83475172c6d800ba134ff0ec058a96a69b1aa
pengwei5 <2943927631@qq.com>
2022-01-10 23:29:51 +0800

    unsafe Alignof

--- unsafe/unsafe.go
+++ unsafe/unsafe.go
@@ -6,6 +6,12 @@ import (
 
 func main() {
 	str := "ab"
-	// Sizeof返回所占用的字节数
+	// 类型本身数据所占用的字节数
 	fmt.Println(unsafe.Sizeof(str))
+
+	// 返回类型的对齐方式（即类型v在内存中占用的字节数）
+	fmt.Println(unsafe.Alignof(str))
+
+	// 它返回该结构起始处与该字段起始处之间的字节数。
+	// fmt.Println(unsafe.Offsetof(str))
 }
